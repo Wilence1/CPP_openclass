b5f51a0db9e349d8c8ea8b5ddb5d9eb6cc95d30d
Wilence1 <wilence.fyh@gmail.com>
2016-03-16 18:30:21 +0300

    Update allocator.cpp

--- allocator.cpp
+++ allocator.cpp
@@ -4,36 +4,28 @@
 #include <iostream>
 #include <stdlib.h>
 #include "allocator.h"
-
 Pointer::Pointer(struct ptr *newelem)
 {
-	tmp = newelem;
-	
+    tmp = newelem;
 }
 Pointer::Pointer()
 {
-	tmp = NULL;
-	
+    tmp = NULL;
 }
-
 void *Pointer::get() const
 {
-	return (tmp->data).base;
+    return (tmp->data).base;
 } 
-
 Allocator::Allocator(void *base, size_t size)
 {
-	this->base = base;
-	this->size = size;
-	list = NULL;
+    this->base = base;
+    this->size = size;
+    list = NULL;
 }
-
-
 Pointer Allocator::alloc(size_t N)
 {
-	struct ptr *q = list;	
-	if(!list)
-	{
+	struct ptr *qmake = list;	
+	if (!list) {
 		struct ptr *newone = (struct ptr *)malloc(sizeof(*newone));
 		newone->next = NULL;
 		newone->data.base = base;
@@ -41,171 +33,120 @@ Pointer Allocator::alloc(size_t N)
 		list = newone;
 		return Pointer(newone);
 	}
-	while(q->next)
-	{
-		if((char *)q->next->data.base - ((char*)q->data.base + q->data.size) >= N)
-		{
+	while (qmake->next) {
+		if((char *)qmake->next->data.base - ((char*)qmake->data.base + qmake->data.size) >= N) {
 			struct ptr *newone = (struct ptr *)malloc(sizeof(*newone));
-			newone->data.base = (char*)q->data.base + q->data.size;
+			newone->data.base = (char*)qmake->data.base + qmake->data.size;
 			newone->data.size = N;
-			newone->next = q->next;
-			q->next = newone;
+			newone->next = qmake->next;
+			qmake->next = newone;
 			return Pointer(newone);
 		}
-		q = q->next;
+		qmake = qmake->next;
 	}
-	if(size + (char *)base - ((char*)q->data.base + q->data.size) >= N)
-	{
+	if(size + (char *)base - ((char*)qmake->data.base + qmake->data.size) >= N) {
 		struct ptr *newone = (struct ptr *)malloc(sizeof(*newone));
-		newone->data.base = (char*)q->data.base + q->data.size;
+		newone->data.base = (char*)qmake->data.base + qmake->data.size;
 		newone->data.size = N;
-		newone->next = q->next;
-		q->next = newone;
+		newone->next = qmake->next;
+		qmake->next = newone;
 		return Pointer(newone);
-
 	}
 	throw AllocError(AllocErrorType::NoMemory, "Wrong alloc.");
 }
-
 void Allocator::free(Pointer &p)
 {
 	struct ptr *x = (p).tmp;
-	
-	struct ptr *q = list;
-	struct ptr *qq = NULL;
-	
-	while(q && q != x)
-	{
-		qq = q;
-		q = q->next;
+	struct ptr *qmake = list;
+	struct ptr *qstep = NULL;
+	while(qmake && qmake != x) {
+		qstep = qmake;
+		qmake = qmake->next;
 	}
-	if(!qq)
-	{
+	if (!qstep ) {
 		list = NULL;
-	}
-	else
-	{
-		qq->next = q->next;
+	} else {
+		qstep ->next = qmake->next;
 	}
 	p.tmp->data.base = 0;
 }
-
 void Allocator::realloc(Pointer &p, size_t N)
 {
-
 	struct ptr *x = (p).tmp;
-	if(!p.tmp)
-	{
+	if(!p.tmp) {
 		p = alloc(N);
 		return;
 	}
-	struct ptr *q = list;
-	struct ptr *qq = NULL;
-	//printf("ira\n");
-	if(q == x)printf("irka");
-	while(q && q != x)
-	{
-		//printf("ira\n");
-		qq = q;
-		q = q->next;
+	struct ptr *qmake = list;
+	struct ptr *qstep = NULL;
+	while(qmake && qmake != x) {
+		qstep = qmake;
+		qmake = qmake->next;
 	}
-	if(!qq)
-	{
-		
-		if(!q->next)
-		{
-			//printf("ira\n");
-			if(size >= N)
-			{
-				for(int i = 0; i < q->data.size; i++)
-				{
-					*((char *)base + i) = *((char*)q->data.base + i);
-					
+	if (!qstep) {
+		if (!qmake->next) {
+			if (size >= N) {
+				for (int i = 0; i < qmake->data.size; i++) {
+					*((char *)base + i) = *((char*)qmake->data.base + i);
 				}
-				q->data.base = base;
-				q->data.size = N;	
+				qmake->data.base = base;
+				qmake->data.size = N;	
 			}
-		}
-		else
-		{
-			if((char *)q->next->data.base - (char*)base >= N)
-			{
-				for(int i = 0; i < q->data.size; i++)
-				{
-					*((char *)base + i) = *((char*)q->data.base + i);
+		} else {
+			if ((char *)qmake->next->data.base - (char*)base >= N) {
+				for(int i = 0; i < qmake->data.size; i++) {
+					*((char *)base + i) = *((char*)qmake->data.base + i);
 				}
-				q->data.base = base;
-				q->data.size = N;
-			}
-			else
-			{
+				qmake->data.base = base;
+				qmake->data.size = N;
+			} else {
 				char *x = (char *)alloc(N).get();
-				for(int i = 0; i < q->data.size; i++)
-				{
-					*((char *)x + i) = *((char*)q->data.base + i);
+				for(int i = 0; i < qmake->data.size; i++) {
+					*((char *)x + i) = *((char*)qmake->data.base + i);
 				}
-				q->data.base = x;
-				q->data.size = N;
-
+				qmake->data.base = x;
+				qmake->data.size = N;
 			}
-			
 		}
-	}
-	else
-	{
-		if(!q->next)
-		{
-			if((char *)base + size - ((char *)q->data.base) >= N)
-			{
-				q->data.size = N;
+	} else {
+		if (!qmake->next) {
+			if ((char *)base + size - ((char *)qmake->data.base) >= N) {
+				qmake->data.size = N;
 			}
-
-		}
-		else
-		{
-			if((char *)q->next->data.base - (char *)q->data.base >= N)
-			{
-				q->data.size = N;
-			}
-			else
-			{
+		} else {
+			if ((char *)qmake->next->data.base - (char *)qmake->data.base >= N) {
+				qmake->data.size = N;
+			} else {
 				char *x = (char *)alloc(N).get();
-				for(int i = 0; i < q->data.size; i++)
-				{
-					*((char *)x + i) = *((char*)q->data.base + i);
+				for(int i = 0; i < qmake->data.size; i++) {
+					*((char *)x + i) = *((char*)qmake->data.base + i);
 				}
-				q->data.base = x;
-				q->data.size = N;
+				qmake->data.base = x;
+				qmake->data.size = N;
 			}
 		}	
 	}
 }
 void Allocator::defrag()
 {
-	struct ptr *q = list;
-	if(q){
-
-	if(!(q->next))
-	{
-		if((char *)q->data.base - (char *)base)
-		{
-			for(int i = 0; i < q->data.size; i++)
-			{
-				*((char *)base + i) = *((char*)q->data.base + i);
+	struct ptr *qmake = list;
+	if (qmake) {
+		if (!(qmake->next)) {
+			if((char *)qmake->data.base - (char *)base) {
+				for(int i = 0; i < qmake->data.size; i++) {
+					*((char *)base + i) = *((char*)qmake->data.base + i);
+				}
+				qmake->data.base = base;
 			}
-			q->data.base = base;
+		}
+		while(qmake && qmake->next) {
+			if((char *)qmake->next->data.base - ((char*)qmake->data.base + qmake->data.size)) {
+				for(int i = 0; i < qmake->next->data.size; i++) {
+					*((char *)qmake->data.base + i) = *((char *)qmake->next->data.base + i);
+				}
+				qmake->next->data.base = (char*)qmake->data.base + qmake->data.size;
+			}
+			qmake = qmake->next;
 		}
 	}
-	while(q && q->next)
-	{
-		if((char *)q->next->data.base - ((char*)q->data.base + q->data.size))
-		{
-			for(int i = 0; i < q->next->data.size; i++)
-			{
-				*((char *)q->data.base + i) = *((char *)q->next->data.base + i);
-			}
-			q->next->data.base = (char*)q->data.base + q->data.size;
-		}
-		q = q->next;
-	}}
 }
